ab0e834815c06602c548501088a103e5215524d1
Syed Adeeb <97741946+SyedAdeebWebDesigning@users.noreply.github.com>
2022-05-20 21:25:02 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,13 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
-
+import Head from 'next/head';
 const Home = () => {
   doucment.title = "Syed Adeeb's Personal Portfolio";
   return (
+    <Head>
+      <title>Syed Adeeb Personal Portfolio<title/>
+    <Head />
     <Layout>
       <Section grid>
         <Hero />
