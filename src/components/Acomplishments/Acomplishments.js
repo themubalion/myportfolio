4a30e4097dca3c0c4cb94e2915f0c958ce5e86a6
SyedAdeebWebDesigning <97741946+SyedAdeebWebDesigning@users.noreply.github.com>
2022-06-04 23:18:36 +0530

    Final

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,7 +4,7 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 40, text: 'Completed Projects'},
+  { number: 40, text: 'Completed Projects' },
   { number: 15, text: 'Big Projects', },
   { number: 190, text: 'Work Stars', },
   { number: 100, text: 'Reviews', }
@@ -12,14 +12,14 @@ const data = [
 
 const Acomplishments = () => (
   <Section>
-    <SectionTitle>Personal Acomplishments</SectionTitle>
+    <SectionTitle>Personal Accomplishments</SectionTitle>
     <Boxes>
-      {data.map((card,index) => (
-        <Box key = {index}>
-          <BoxNum>{card.number}+</BoxNum>
-          <BoxText>{card.text}</BoxText>
+      { data.map((card, index) => (
+        <Box key={ index }>
+          <BoxNum>{ card.number }+</BoxNum>
+          <BoxText>{ card.text }</BoxText>
         </Box>
-      ))}
+      )) }
     </Boxes>
     <SectionDivider />
   </Section>
